26094fa2a50eae7be20ccb469fbda30c224bca9b
tsusu0409 <tsubasa.kawagishi@gamil.com>
2025-05-11 22:18:18 +0900

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-    const [count, setCount] = useState(0);
 
     return(
         <div>
-
+            <Header />
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import './Header.css';
+
+function Header() {
+
+    return(
+        <div className='header-container'>
+            <div className='header-title'>
+                
+            </div>
+            <div className='haeder-logo'>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Header;
